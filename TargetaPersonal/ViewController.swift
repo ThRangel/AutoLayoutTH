24d03edc9a3492b12940831ec6453561fb20c634
ThRangel <the.beatles.lma2@gmail.com>
2020-08-28 22:03:36 -0500

    Targeta Personal con AutoLayout

---
+++ TargetaPersonal/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TargetaPersonal
+//
+//  Created by MacBook Air  on 23/06/20.
+//  Copyright © 2020 MacBook Air . All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
